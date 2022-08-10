0d44983f4a32ca947baf3585f37af8448ff6a309
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-08-10 20:50:55 -0300

    chore:adapting folder routes

--- src/services/folderServices.ts
+++ src/services/folderServices.ts
@@ -8,6 +8,16 @@ export async function getAllFolders(userId : number){
     const result =  await folderRepository.getAllFolders(userId)
     return result
 }
+export async function compareUserFolder(userId: number, folderId: number) {
+    const folder = await getFolderById(folderId)
+    if (folder.userId !== userId) {
+        throw {
+            type: "not_found",
+            message: "folder does not belong to the user"
+        }
+    }
+    return folder
+}
 export async function getFolderById(id: number) {
     return folderRepository.getFolderById(id)
 }
--- src/services/subjectServices.ts
+++ src/services/subjectServices.ts
@@ -1,18 +1,8 @@
 import { CreateSubjectData } from "../interfaces/interfaces.js"
 
 import * as subjectRepository from "../repositories/subjectRepository.js"
-import * as folderServices from "../services/folderServices.js"
 export async function insertSubjectDb(newSubject: CreateSubjectData) {
     await subjectRepository.insertSubjectDb(newSubject)
-}
-export async function compareUserFolder(userId: number, folderId: number) {
-    const folder = await folderServices.getFolderById(folderId)
-    if (folder.userId !== userId) {
-        throw {
-            type: "not_found",
-            message: "folder does not belong to the user"
-        }
-    }
 }
 export async function getSubjectById(id: number) {
     return await subjectRepository.getSubjectById(id)
